dedaa5d9fe6463d5ffe5e68b76bb09e093a88d78
admin <admin@DESKTOP-3PE3QU4>
2021-08-06 15:47:18 +0900

    4장 예제까지

---
+++ testWeb/src/main/java/ch04/com/dao/Person.java
@@ -0,0 +1,29 @@
+package ch04.com.dao;
+
+public class Person {
+	private int id;
+	private String name;
+
+	public Person() {
+		id = 20181004;
+		name = "È«±æ¼ø";
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+}
